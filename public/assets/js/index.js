8ee1eb33bd1b995152355ae06d52b4f0cdbe4db0
MJerich <MatthewJerich@gmail.com>
2022-04-24 12:43:30 -0700

    add heroku

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,8 +4,6 @@ let saveNoteBtn;
 let newNoteBtn;
 let noteList;
 
-console.log(noteList);
-
 if (window.location.pathname === '/notes') {
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
@@ -28,7 +26,7 @@ const hide = (elem) => {
 let activeNote = {};
 
 const getNotes = () =>
-  fetch('/api/notes', {
+  fetch('/api/notes?', {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -36,7 +34,7 @@ const getNotes = () =>
   });
 
 const saveNote = (note) =>
-  fetch('/api/notes', {
+  fetch('/api/notes?', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
